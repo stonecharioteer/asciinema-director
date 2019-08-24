7d50e64b859d0122cba18046a9637787d629f3a0
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-08-24 21:12:46 +0530

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Vinay Keerthi'
 author = 'Vinay Keerthi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
