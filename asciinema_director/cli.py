c28b34391947fd7f8bc4dc64e27d9391deb34249
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-08-24 20:49:58 +0530

    fix type

--- asciinema_director/cli.py
+++ asciinema_director/cli.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
+"""Command line interface.
 """
 
 import click
 
 
 @click.command("asciinema-director")
-@click.option("--delay", "-D", type=click.Float, default=0.01)
+@click.option("--delay", "-D", type=click.FLOAT, default=0.01)
 @click.argument("src")
 @click.argument("dest")
 def cli(delay, src, dest):
