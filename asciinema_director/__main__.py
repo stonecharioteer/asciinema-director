ac150eb5ba1326344cb8133095ef6c2ec2fef093
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-08-24 20:36:24 +0530

    add __main__ file for easier use

---
+++ asciinema_director/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""__main__ file to enable use like `python -m asciinema_director`"""
+
+
+from asciinema_director.cli import cli
+
+if __name__ == "__main__":
+    cli()
